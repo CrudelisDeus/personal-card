d3975139d6b7b2e116972915efd326d0ee44950c
Dmytro <karakurt.deus@gmail.com>
2025-12-04 00:07:00 +0200

    added

--- static/js/main.js
+++ static/js/main.js
@@ -36,43 +36,43 @@ document.addEventListener("DOMContentLoaded", () => {
     setHrefForAll('.btn-contact-me', LINK_CONTACT_ME);
 });
 
-function animateCounter(element, target, durationMs = MAX_ANIMATION_DURATION, suffix = "+") {
-    if (!element) return;
+// function animateCounter(element, target, durationMs = MAX_ANIMATION_DURATION, suffix = "+") {
+//     if (!element) return;
 
-    element.innerHTML = `<span class="counter">0${suffix}</span>`;
-    const counter = element.querySelector(".counter");
+//     element.innerHTML = `<span class="counter">0${suffix}</span>`;
+//     const counter = element.querySelector(".counter");
 
-    const steps = target;
-    let value = 0;
+//     const steps = target;
+//     let value = 0;
 
-    let weights = [];
-    let totalWeight = 0;
+//     let weights = [];
+//     let totalWeight = 0;
 
-    for (let i = 0; i < steps; i++) {
-        let w = Math.random();
-        weights.push(w);
-        totalWeight += w;
-    }
+//     for (let i = 0; i < steps; i++) {
+//         let w = Math.random();
+//         weights.push(w);
+//         totalWeight += w;
+//     }
 
-    let accumulated = 0;
-    for (let i = 0; i < steps; i++) {
-        let delay = durationMs * (weights[i] / totalWeight);
-        accumulated += delay;
+//     let accumulated = 0;
+//     for (let i = 0; i < steps; i++) {
+//         let delay = durationMs * (weights[i] / totalWeight);
+//         accumulated += delay;
 
-        setTimeout(() => {
-            value++;
+//         setTimeout(() => {
+//             value++;
 
-            counter.classList.add("counter-changing");
+//             counter.classList.add("counter-changing");
 
-            setTimeout(() => {
-                counter.textContent = value + suffix;
+//             setTimeout(() => {
+//                 counter.textContent = value + suffix;
 
-                counter.classList.remove("counter-changing");
+//                 counter.classList.remove("counter-changing");
 
-            }, 120);
-        }, accumulated);
-    }
-}
+//             }, 120);
+//         }, accumulated);
+//     }
+// }
 
 document.addEventListener("DOMContentLoaded", () => {
     animateCounter(document.querySelector('.main-body-left-yc-1 h1'), YEARS_EXPERIENCE);

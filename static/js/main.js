a7a8907ada6c5b5474fd0ab2b4bd016852dd7aa9
Dmytro <karakurt.deus@gmail.com>
2025-12-04 23:31:12 +0200

    added cv

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,4 @@
+
 const LINK_ABOUT_ME = "./index.html";
 const LINK_SKILLS = "test1";
 const LINK_RESUME = "./cv.html";
@@ -7,6 +8,12 @@ const LINK_CONTACT_ME = "https://t.me/CrudelisDeus";
 const YEARS_EXPERIENCE = 5;
 const COMPLETED_PROJECTS = 8;
 const MAX_ANIMATION_DURATION = 2000;
+
+// CV
+const LAST_UPDATE = "12/2025";
+const LINK_DOWNLOAD = "./index.html";
+const LINK_WEB_VIEW = "./index.html";
+
 
 document.addEventListener("DOMContentLoaded", () => {
     const mobile = document.querySelector('.mobile2');
@@ -34,47 +41,23 @@ document.addEventListener("DOMContentLoaded", () => {
     setHrefForAll('.btn-resume', LINK_RESUME);
     setHrefForAll('.btn-contacts', LINK_CONTACTS);
     setHrefForAll('.btn-contact-me', LINK_CONTACT_ME);
+    setHrefForAll('.btn-cv-download', LINK_DOWNLOAD);
+    setHrefForAll('.btn-cv-web-view', LINK_WEB_VIEW);
 });
-
-// function animateCounter(element, target, durationMs = MAX_ANIMATION_DURATION, suffix = "+") {
-//     if (!element) return;
-
-//     element.innerHTML = `<span class="counter">0${suffix}</span>`;
-//     const counter = element.querySelector(".counter");
-
-//     const steps = target;
-//     let value = 0;
-
-//     let weights = [];
-//     let totalWeight = 0;
-
-//     for (let i = 0; i < steps; i++) {
-//         let w = Math.random();
-//         weights.push(w);
-//         totalWeight += w;
-//     }
-
-//     let accumulated = 0;
-//     for (let i = 0; i < steps; i++) {
-//         let delay = durationMs * (weights[i] / totalWeight);
-//         accumulated += delay;
-
-//         setTimeout(() => {
-//             value++;
-
-//             counter.classList.add("counter-changing");
-
-//             setTimeout(() => {
-//                 counter.textContent = value + suffix;
-
-//                 counter.classList.remove("counter-changing");
-
-//             }, 120);
-//         }, accumulated);
-//     }
-// }
 
 document.addEventListener("DOMContentLoaded", () => {
     animateCounter(document.querySelector('.main-body-left-yc-1 h1'), YEARS_EXPERIENCE);
     animateCounter(document.querySelector('.main-body-left-yc-2 h1'), COMPLETED_PROJECTS);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    animateCounter(document.querySelector('.btn-cv-download'), LINK_DOWNLOAD);
+    animateCounter(document.querySelector('.btn-cv-web-view'), LINK_WEB_VIEW);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const updateElement = document.querySelector('.cv-update');
+    if (updateElement) {
+        updateElement.textContent = `Update: ${LAST_UPDATE}`;
+    }
 });
